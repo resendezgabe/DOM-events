bdc2d854f06a29310ea9476e37492d8c0c3b0e56
gabriel <resendez.gabe@yahoo.com>
2023-03-22 17:28:30 -0400

    Update index.js

--- craneGame/index.js
+++ craneGame/index.js
@@ -8,20 +8,20 @@ let cranePosition = { top: 50, left: 50 };
 
 upButton.addEventListener("click", function() {
     cranePosition.top -= 10;
-    crane.style.top = "${craneposition.top}%";
+    crane.style.top = `${craneposition.top}%`;
 });
 
 leftButton.addEventListener("click", function() {
     cranePosition.left -= 10;
-    crane.style.left = "${cranePosition.left}%";
+    crane.style.left = `${cranePosition.left}%`;
 });
 
 rightButton.addEventListener("click", function() {
     cranePosition.left += 10;
-    crane.style.left = "${cranePosition.left}%";
+    crane.style.left = `${cranePosition.left}%`;
 });
 
 downButton.addEventListener("click", function() {
     cranePosition.top += 10;
-    crane.style.top = "${cranePosition.top}%";
+    crane.style.top = `${cranePosition.top}%`;
 });
