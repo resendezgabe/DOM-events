318a1f1a3ecc7f443fac1ab5c5f4012cd40ef840
gabriel <resendez.gabe@yahoo.com>
2023-03-22 15:45:35 -0400

    Update index.js

--- magnifyingGlass/index.js
+++ magnifyingGlass/index.js
@@ -2,8 +2,10 @@ const textEl = document.getElementById("text");
 const increaseFontSizeBtn = document.getElementById("increase-font-size");
 const decreaseFontSizeBtn = document.getElementById("decrease-font-size");
 
+//defines value of fontSize
 let fontSize = 16;
 
+//increases the font size of the text when button is clicked
 increaseFontSizeBtn.addEventListener("click", function() {
   if (fontSize < 100) {
     fontSize += 2;
@@ -11,6 +13,7 @@ increaseFontSizeBtn.addEventListener("click", function() {
   }
 });
 
+//decreases the font size of text when button is clicked
 decreaseFontSizeBtn.addEventListener("click", function() {
   if (fontSize > 1) {
     fontSize -= 2;
